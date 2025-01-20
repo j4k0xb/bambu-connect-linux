2e96c6fd093d06dcfa736cb5e0eff5232a1d4682
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2025-01-20 02:34:04 +0100

    fix url scheme

--- forge.config.ts
+++ forge.config.ts
@@ -4,26 +4,21 @@ import { MakerAppImage } from "@reforged/maker-appimage";
 
 export const packagerConfig = {
   asar: true,
+  icon: "bambu-connect",
+  protocols: [
+    {
+      name: "bambu-connect",
+      schemes: ["bambu-connect"],
+    },
+  ],
 };
 export const rebuildConfig = {};
 export const makers = [
   new MakerAppImage({
     options: {
-      // Package name.
-      name: "bambu-connect",
-      // Executable name.
-      bin: "bambu-connect",
-      // Human-friendly name of the application.
-      productName: "Bambu Connect (Beta)",
-      // `GenericName` in generated `.desktop` file.
-      genericName: "Bambu Connect",
-      // Path to application's icon.
-      icon: "icon.png",
-      // Desktop file to be used instead of the configuration above.
-      // desktopFile: "/path/to/example-app.desktop",
-      // Release of `AppImage/AppImageKit`, either number or "continuous".
+      icon: "bambu-connect.png",
+      desktopFile: "bambu-connect.desktop",
       AppImageKitRelease: "continuous",
-      // Support parsing Arch Linux '*_flags.conf' file.
       flagsFile: true,
     },
   }),
